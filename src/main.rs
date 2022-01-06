4759406a5af6001aed54e842c21188d88739e405
Dennis ten Hoove <dennistenhoove@protonmail.com>
2022-01-06 05:39:27 +0100

    Utilize render and print output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod board;
 
 fn main() {
-    board::init();
+    let board_layout = board::init();
+
+    let board_current_render = board::render(&board_layout);
+    println!("{}", board_current_render)
 }
